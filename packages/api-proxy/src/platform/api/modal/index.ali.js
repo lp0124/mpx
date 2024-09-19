167b5a4297cdb906f1b909b49c1d53dd305fcaa8
lp0124 <568687535@qq.com>
2024-09-19 14:58:00 +0800

    feat(qmai): api-proxy showModal 定制修改 点击取消按钮时走 catch 逻辑, 点击确认按钮走 then

--- packages/api-proxy/src/platform/api/modal/index.ali.js
+++ packages/api-proxy/src/platform/api/modal/index.ali.js
@@ -13,6 +13,13 @@ function showModal (options = {}) {
       return changeOpts(res, undefined, { cancel: !res.confirm })
     })
 
+    const cacheFail = opts.fail
+    const cacheSuccess = opts.success
+    opts.success = function (res) {
+      if (!res.confirm) cacheFail.call(this)
+      else cacheSuccess.call(this, res)
+    }
+
     return ENV_OBJ.confirm(opts)
   } else {
     opts = changeOpts(options, {
--- packages/api-proxy/src/platform/api/modal/index.js
+++ packages/api-proxy/src/platform/api/modal/index.js
@@ -1,6 +1,14 @@
-import { ENV_OBJ, envError } from '../../../common/js'
+import { ENV_OBJ } from '../../../common/js'
 
-const showModal = ENV_OBJ.showModal || envError('showModal')
+function showModal (options = {}) {
+  const cacheFail = options.fail
+  const cacheSuccess = options.success
+  options.success = function (res) {
+    if (!res.confirm) cacheFail.call(this)
+      else cacheSuccess.call(this, res)
+  }
+  ENV_OBJ.showModal(options)
+}
 
 export {
   showModal
